827e8f310d7f2f3948b426a547f2d99ff4d2ee0e
julian1 <julian.atkinson71@gmail.com>
2015-03-04 17:44:51 +1100

    update

--- test3.java
+++ test3.java
@@ -15,30 +15,57 @@ import java.util.Properties;
 
 public class test3 {
 
+
+
+    public static void fuck ( Connection conn )  throws Exception
+	{
+		Statement stmt = conn.createStatement(); 
+		ResultSet rs = stmt.executeQuery( "SELECT * FROM anmn_ts.measurement limit 1" );
+
+		while ( rs.next() ) {
+			int numColumns = rs.getMetaData().getColumnCount();
+			for ( int i = 1 ; i <= numColumns ; i++ ) {
+			   // Column numbers start at 1.
+			   // Also there are many methods on the result set to return
+			   //  the column as a particular type. Refer to the Sun documentation
+			   //  for the list of valid conversions.
+			   System.out.println( "COLUMN " + i + " = " + rs.getObject(i) );
+			}
+		}
+	
+	}
+
+
     public static void main(String[] args)  throws Exception
 	{
 
-		String url = "jdbc:postgresql://127.0.0.1/postgres";
+		//String url = "jdbc:postgresql://127.0.0.1/postgres";
+		String url = "jdbc:postgresql://dbprod.emii.org.au/harvest";
 		Properties props = new Properties();
-		props.setProperty("user","meteo");
-		props.setProperty("password","meteo");
+		props.setProperty("user","jfca");
+		props.setProperty("password","fredfred");
 		props.setProperty("ssl","true");
 		props.setProperty("sslfactory","org.postgresql.ssl.NonValidatingFactory");
 
 		props.setProperty("driver","org.postgresql.Driver" );
 
-	
 
 		Connection conn = DriverManager.getConnection(url, props);
 		if( conn != null ) {
 
 			System.out.println( "got conn" );
+			
+			fuck( conn );
 
 		}
 
-		// String url = "jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=true";
-		// Connection conn = DriverManager.getConnection(url);
 	}
 }
+
+//			Statement stmt = conn.createStatement(); 
+		//	ResultSet rs = stmt.executeQuery( "SELECT * FROM anmn_ts.measurement limit 1" );
+		// String url = "jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=true";
+		// Connection conn = DriverManager.getConnection(url);
+
 
 
