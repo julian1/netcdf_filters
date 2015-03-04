f5f6446ac72364f6aae890a82a554ff2cb4d1336
julian1 <julian.atkinson71@gmail.com>
2015-03-04 13:39:20 +1100

    update

--- test2.java
+++ test2.java
@@ -1,18 +1,18 @@
 
 // rm *.class ; javac main.java  ; java main
 
-//import java.io.BufferedReader; 
-import java.io.FileReader; 
-import java.io.InputStream ; 
+//import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStream ;
 
 import java.io.FileInputStream;
-import java.io.BufferedInputStream; 
+import java.io.BufferedInputStream;
 
 //import java.util.StringTokenizer;
 
-// string tokenizer isn't going to work because may not be strings. 
+// string tokenizer isn't going to work because may not be strings.
 
-// do everything twice? 
+// do everything twice?
 // or leave the value there...
 
 interface IExpression
@@ -53,15 +53,15 @@ class ExprIdentifier implements IExpression
 
 
 
-// actually why not pass a 
-// string s, integer pos, boxed type....  
+// actually why not pass a
+// string s, integer pos, boxed type....
 
 class Context
 {
 
-/*	public Context( String s, int pos) 
+/*	public Context( String s, int pos)
 	{
-		int pos2 = parseExpression( s, pos ); 
+		int pos2 = parseExpression( s, pos );
 	}
 
 	So i think we have to parse the thing, and store the pos in the
@@ -69,46 +69,48 @@ class Context
 */
 
 	// | Int
-	// | f '(' expr_list ')'   f 
-	// | expr_list = 
-	//			expr 
-	// | '(' expr, expr ')'   tuple 
+	// | f '(' expr_list ')'   f
+	// | expr_list =
+	//			expr
+	// | '(' expr, expr ')'   tuple
 
 	IExpression parseExpression(String s, int pos)
 	{
 		// actually should try to glob up
-		
+
 		// try whitespace
 		// try integer
-		ExprInteger expr = parseInt(s, pos); 
+		ExprInteger expr = parseInt(s, pos);
 		if(expr != null)
 		{
 			// append
-			//System.out.println("whoot got integer!" + expr ); 
+			//System.out.println("whoot got integer!" + expr );
 			//pos = expr.pos;
 			return expr;
 		}
 
-		ExprIdentifier expr2 = parseIdentifier(s, pos); 
+		ExprIdentifier expr2 = parseIdentifier(s, pos);
 		if( expr2 != null)
 		{
 
 
-		} 
+		}
 		return null;
 	}
 
 
+	// a tuple is just an unnamed s-expression, i don't think we even really need it.
 
 	ExprIdentifier parseIdentifier( String s, int pos)
 	{
-		if(Character.isLetter(s.charAt(pos))) {
-//			while(Character.isSpaceChar(s.charAt(pos))) {
-//				++pos;
-			}	
-
+		if(Character.isLetter(s.charAt(pos))  ) {
+			StringBuilder b = new StringBuilder();
+			while(Character.isLetter(s.charAt(pos)) || s.charAt(pos) == '_') {
+				b.append(s.charAt(pos));
+				++pos;
+			}
 		}
-		return null;
+
 
 
 		return null;
@@ -120,7 +122,7 @@ class Context
 		if(Character.isSpaceChar(s.charAt(pos))) {
 			while(Character.isSpaceChar(s.charAt(pos))) {
 				++pos;
-			}	
+			}
 			return new ExprWhite(pos);
 		}
 		return null;
@@ -129,15 +131,15 @@ class Context
 	ExprInteger parseInt( String s, int pos)
 	{
 		// we are committed
-		if(Character.isDigit(s.charAt(pos))) {  
-			StringBuilder buf = new StringBuilder();
+		if(Character.isDigit(s.charAt(pos))) {
+			StringBuilder b = new StringBuilder();
 			while( Character.isDigit(s.charAt(pos))) {
-				buf.append(s.charAt(pos));
+				b.append(s.charAt(pos));
 				++pos;
-			}	
-			int value = Integer.parseInt(buf.toString());
-			System.out.println( "whoot integer "  + Integer.toString( value )  ); 
-			return new ExprInteger( pos, value); 
+			}
+			int value = Integer.parseInt(b.toString());
+			System.out.println( "whoot integer "  + Integer.toString( value )  );
+			return new ExprInteger( pos, value);
 		}
 		return null;
 	}
@@ -148,18 +150,18 @@ class Context
 
 // or we tokenize the
 // Why not try to do it recursively
-// to return a tuple means we lose the typing if we use an integer 
+// to return a tuple means we lose the typing if we use an integer
 // if it succeeds then we return the value
-// it maybe that we want to not use Integer but instead an expression . ExprInteger or IntegerExpression 
+// it maybe that we want to not use Integer but instead an expression . ExprInteger or IntegerExpression
 // or TupleExpression
 
 
 
 public class test2 {
 
-    public static void main(String[] args) 
+    public static void main(String[] args)
 	{
-		String s = "777 and ( contains(geom, box( (0,0), ... ), less ( time , 1.1.2015 )"; 
+		String s = "777 and ( contains(geom, box( (0,0), ... ), less ( time , 1.1.2015 )";
 
 
 		Context c = new Context();// s, 0 );
@@ -168,10 +170,10 @@ public class test2 {
 /*
 		int pos = 0;
 
-		if(s.charAt(pos) >= '0' &&  s.charAt(pos) <= '9') {  
+		if(s.charAt(pos) >= '0' &&  s.charAt(pos) <= '9') {
 			while(s.charAt(pos) >= '0' &&  s.charAt(pos) <= '9') {
 				++pos;
-			}	
+			}
 		}
 		else {
 			System.out.println( "not an integer !"  ); // Display the string.
